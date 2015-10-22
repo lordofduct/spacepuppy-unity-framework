48beb7df639b51c9c94cf8d924440b26dca73209
lordofduct <lordofduct@gmail.com>
2015-10-22 01:55:03 -0400

    Bug fix in Cameras library, forgot to include SPAssets.cs

---
+++ SpacepuppyBase/SPAssets.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace com.spacepuppy
+{
+
+    public static class SPAssets
+    {
+
+        #region Fields
+
+        private static Texture2D _whiteTexture;
+
+        #endregion
+
+        #region Properties
+
+        public static Texture2D WhiteTexture
+        {
+            get
+            {
+                if(_whiteTexture == null)
+                {
+                    _whiteTexture = new Texture2D(1, 1);
+                    _whiteTexture.SetPixel(0, 0, Color.white);
+                    _whiteTexture.Apply();
+                }
+                return _whiteTexture;
+            }
+        }
+
+        #endregion
+
+    }
+
+}
